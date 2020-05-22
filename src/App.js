30d21e96b67b8db080e28ba457f9e23e46ff27ad
“kbefff” <kbeffel10@me.com>
2020-05-22 10:35:02 -0700

    preview page styling

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 import {Switch, Route} from 'react-router-dom'
 
 
+
 import HomePage from './pages/homepage/homepage.component'
-const HatsPage = () =>
- (
-   <div>
-     <h1>Hats Page</h1>
-   </div>
- )
+import ShopPage from './pages/shop/shop.component'
+
+
 
 function App() {
   return (
@@ -18,7 +16,7 @@ function App() {
       Only one thing will render */}
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
 
     </div>
