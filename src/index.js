2fc2aab53a2069485200d3ef772573950824f65c
“Kelsey <kbeffel10@me.com>
2020-05-30 14:38:34 -0700

    build store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { BrowserRouter}  from 'react-router-dom'
 import { Provider } from 'react-redux;' // a component we want to wrap around the entire app because we want 
 //everything in the app to have acess to the store object we get from react
 // the provide is a component that is the parent of everythign in the app.
 
 
+import './index.css';
+import App from './App';
+import store from '.redux/store'
+
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+// the middleware between action and root recuder ar functions that recieve objets, do something with them, then pass them out
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger'; // makes it easier to understand what we're doing
+
+import rootReducer from './root-reducer';
+
+// setup middleware
+// the middleware the store is expecting from us is going to be an array
+const middlewares = [logger];
+
+// spread all items in middleware into applyMiddleware function
+const store = createStore(rootReducer, applyMiddleware(...middlewares))
