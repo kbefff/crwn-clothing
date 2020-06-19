59b3b2855aecb0b8099cc26b118bd0ce75a5d527
“Kelsey <kbeffel10@me.com>
2020-06-18 20:23:44 -0700

    toggle cart hidden on checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -9,7 +9,7 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className = 'cart-dropdown'>
         <div className='cart-items'>
             {
@@ -20,7 +20,10 @@ const CartDropdown = ({ cartItems, history }) => (
                 : <span className='empty-message'>Your cart is empty</span>
             }
         </div>
-        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+            history.push('/checkout')
+            dispatch(toggleCartHidden())
+        }}>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
