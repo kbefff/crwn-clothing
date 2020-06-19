d6ed665a123129d120339dae4f270923b504569d
“Kelsey <kbeffel10@me.com>
2020-06-19 16:27:28 -0700

    add checkout item

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
+
+import CheckoutItem from '../../components/checkout-item/checkout-item.component'
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 
 import './checkout.styles.scss';
@@ -26,8 +28,8 @@ const CheckoutPage = ({ cartItems, total}) => (
             </div>
         </div>
         {
-            cartItems.map(cartItems => 
-            cartItems.name  
+            cartItems.map(cartItem => 
+            <CheckoutItem key={cartItems.id} cartItem={cartItem}  />  
                 )
         }
         <div className='total'>
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -2,7 +2,7 @@ import CartActionTypes from './cart.types';
 
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
-})
+});
 
 export const addItem = item => ({
     type: CartActionTypes.ADD_ITEM,
