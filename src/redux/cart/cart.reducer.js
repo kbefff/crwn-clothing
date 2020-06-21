f6a0081b6ca2e80679dac7a12c7205f11de7c64f
“Kelsey <kbeffel10@me.com>
2020-06-20 18:53:35 -0700

    create clear item reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,6 @@
 import CartActionTypes from './cart.types'
 import { addItemToCart } from './cart.utils';
 
-
 const INITIAL_STATE = {
     hidden: true,
     cartItems: [] //add items into array or clear them out depending on the feature;
@@ -14,14 +13,21 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
             hidden: !state.hidden
         };
-        default:
-            return state;
         case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id
+                    //if cart item id does not match the item we want, keep
+                    )
             }
-
+        default:
+            return state;
     }
 };
 
