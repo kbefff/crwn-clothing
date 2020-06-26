62b790d6e2dc1dcc9ed2face5c01d647edb259bc
“Kelsey <kbeffel10@me.com>
2020-06-26 16:32:37 -0700

    shop component and reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,23 @@
 import React from 'react';
-import SHOP_DATA from './shop.data'
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component'
 
+import {selectCollections} from '../../redux/shop/shop.selectors';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
+const ShopPage =({ collections }) => (
+    <div className ='shop-page'>
+        {
+            collections.map(({id, ...otherColletionProps}) => (
+                <CollectionPreview key ={id}{...otherColletionProps} />
+            ))
         }
-    }
+    </div>
+);
 
-    render(){
-    const {collections} = this.state;
-      return (<div className ='shop-page'>
-          {
-              collections.map(({id, ...otherColletionProps}) => (
-                  <CollectionPreview key ={id}{...otherColletionProps} />
-              ))
-          }
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
 
-      </div>);
-    } 
-}
-
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -6,6 +6,7 @@ import storage from 'redux-persist/lib/storage'; //this gives us the actual loca
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
 const persistConfig = {
     key: 'root', //we want to start storing things at the root
@@ -17,7 +18,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
-    directory: directoryReducer
+    directory: directoryReducer,
+    shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
