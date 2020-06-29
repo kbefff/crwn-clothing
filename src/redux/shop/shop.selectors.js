38faf4038022d28b1332f40c7e5388c8d8542329
“Kelsey <kbeffel10@me.com>
2020-06-29 09:53:40 -0700

    colletion routing and selector

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,23 @@
 import { createSelector } from 'reselect';
 
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackers: 3,
+    womens: 4,
+    mens: 5
+} //pass in url string and use as dynmaic value
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
+)
+
+export const selectCollection = collectionUrlParam => 
+createSelector(
+    [selectCollections],
+    collections => collections.find(
+        collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
 )
