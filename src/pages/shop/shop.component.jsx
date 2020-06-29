85f27b3df193ade01d9fb60a865e618876c6cb14
“Kelsey <kbeffel10@me.com>
2020-06-29 08:56:55 -0700

    move collections into own componenet

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
 
-import {selectCollections} from '../../redux/shop/shop.selectors';
 
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 const ShopPage =({ collections }) => (
     <div className ='shop-page'>
         {
             collections.map(({id, ...otherColletionProps}) => (
-                <CollectionPreview key ={id}{...otherColletionProps} />
+                <CollectionsOverview key ={id}{...otherColletionProps} />
             ))
         }
     </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-})
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
