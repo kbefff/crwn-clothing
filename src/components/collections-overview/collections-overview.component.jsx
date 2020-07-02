2791d82c57de97da1dff512f5189b90e8697600d
“Kelsey <kbeffel10@me.com>
2020-07-01 18:38:02 -0700

    update colletions overview

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -8,18 +8,16 @@ import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import './collections-overview.styles.scss';
 
-
-
 const CollectionsOverview = ({ collections }) => (
-    <div className='collections-overview'>
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
-    </div>
-  );
+  <div className='collections-overview'>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollectionsForPreview
-})
+  collections: selectCollectionsForPreview
+});
 
-export default connect(mapStateToProps)(CollectionsOverview)
+export default connect(mapStateToProps)(CollectionsOverview);
