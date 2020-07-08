2cfbc2b78907b79ede1a8e7cf82c02beaf322656
“Kelsey <kbeffel10@me.com>
2020-07-07 18:48:19 -0700

    some formatting

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import CustomButton from '../custom-button/custom-button.component'
-import CartItem from '../cart-item/cart-item.component'
-import { selectCartItems } from '../../redux/cart/cart.selectors'
+import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { createStructuredSelector } from 'reselect';
-import { toggleCartHidden } from '../../redux/cart/cart.actions'
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-import './cart-dropdown.styles.scss'
+import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className = 'cart-dropdown'>
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,22 +2,15 @@ import React from 'react';
 
 import './custom-button.styles.scss'
 
-
-
 // this is going to be a presentational componenet --> a functional componenet with no state
 // we are building as own componenet so that it can be used in any other component that might need
 // consistent componenet styling across the project
 
 const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => (
-    <button 
-        className=
-        {
-            `${inverted ? 'inverted' : ''} 
-            ${isGoogleSignIn ? 'google-sign-in' : ''} 
-        custom-button`} 
+    <button className=
+        {`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} 
         {...otherProps}>
         {children}
-
     </button>
 )
 
