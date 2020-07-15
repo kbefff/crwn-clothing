53c5288c714ff7d491588ab7609efde6960850b6
“Kelsey <kbeffel10@me.com>
2020-07-15 09:31:08 -0700

    change favicon and remove underline from links

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux'; //higher order component that lets us have access to things related to redux
-//higher order takes components as arguments and returns a new suped up component
+import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { auth } from '../../firebase/firebase.utils';
@@ -9,46 +8,41 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selector';
-// import CartIcon from '../cart-icon/cart-icon.component'
-// we are setting it to the logo key word
-import {ReactComponent as Logo} from '../../assets/crown.svg';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 import './header.styles.scss';
 
-//functional component
 const Header = ({ currentUser, hidden }) => (
-    // div that contains header code
-    <div className='header'> 
-        {/* logo that links to hompage on click */}
-        {/* link to homepage */}
-        {/* svg logo, really small, really performant, 
-        looks the same no matter what the size */}
-        <Link className='logo-container' to="/">
-            <Logo className='logo'/>
-        </Link>
-            <div className='options'>
-                <Link className='option' to='/shop'>
-                    SHOP
-                </Link>
-                <Link className='option' to='/shop'>
-                    CONTACT
-                </Link>
-                { currentUser ? (
-                    <div className='option' onClick={() => auth.signOut()}>
-                        SIGN OUT
-                    </div>
-                ) : (
-                    <Link className='option' to='/signIn'>SIGN IN</Link>
-                )}
-                <CartIcon />
-            </div>
-            { hidden ? null : <CartDropdown />}
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
+      <CartIcon />
+    </div>
+    {hidden ? null : <CartDropdown />}
+  </div>
 );
 
-const mapStateToProps =  createStructuredSelector({ // the state is the top level root reducet
-    currentUser: selectCurrentUser,
-    hidden: selectCartHidden
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 });
-
 
 export default connect(mapStateToProps)(Header);
